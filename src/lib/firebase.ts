0ed6c8651ab837670cb9c904c93c814bec103032
TikTok Id <tid441184@gmail.com>
2025-08-25 03:14:44 +0000

    const firebaseConfig = {   apiKey: "AIzaSyD_Wjl-gchuuSc09toj1NXgZaY68gMZ

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,10 +6,10 @@ import { getAuth } from "firebase/auth";
 import { collection, addDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5sB_Gk-v4A1b2c3d4e5f6g7h8i9j0k",
+  apiKey: "AIzaSyD_Wjl-gchuuSc09toj1NXgZaY68gMZiNg",
   authDomain: "save-genie-video-downloader.firebaseapp.com",
   projectId: "save-genie-video-downloader",
-  storageBucket: "save-genie-video-downloader.appspot.com",
+  storageBucket: "save-genie-video-downloader.firebasestorage.app",
   messagingSenderId: "713351902363",
   appId: "1:713351902363:web:8c5e49dcf9b605e400023e"
 };
