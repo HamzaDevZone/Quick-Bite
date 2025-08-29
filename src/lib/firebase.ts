57cebd3ddc59e8c88723ccd2c6905324ea062807
TikTok Id <tid441184@gmail.com>
2025-08-29 15:20:02 +0000

    Baki ki 6 files ko bi check karo or oska bad 116 files ko recheck karo

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyD_Wjl-gchuuSc09toj1NXgZaY68gMZiNg",
-  authDomain: "save-genie-video-downloader.firebaseapp.com",
-  projectId: "save-genie-video-downloader",
-  storageBucket: "save-genie-video-downloader.firebasestorage.app",
-  messagingSenderId: "713351902363",
-  appId: "1:713351902363:web:8c5e49dcf9b605e400023e"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
