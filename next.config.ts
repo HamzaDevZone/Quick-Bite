9946bd97ba71fb707e391608cd091742d9e8273f
TikTok Id <tid441184@gmail.com>
2025-08-30 18:51:23 +0000

    Ni firebase ka ilawa external hosting personal domin ka Sath or Isa thk

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
